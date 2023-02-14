5cea27eb9b19666865245ed37d4dc6eb5e3269cb
ZEGHAR <mohamed-lamine.ZEGHAR@etu.univ-amu.fr>
2023-02-14 16:37:14 +0100

    l'interface graphique lyricsapps.fxml

--- src/main/java/app/lyricsapp/LyricsApp.java
+++ src/main/java/app/lyricsapp/LyricsApp.java
@@ -11,8 +11,26 @@ public class LyricsApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/app/lyricsapp/view/lyricsapp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/app/lyricsapp/view/lyricsapp1.fxml"));
         primaryStage.setTitle("LyricsApp");
+        /*GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        Label label = new Label("Speech: ");
+        GridPane.setConstraints(label, 0, 0);
+
+        TextField speechInput = new TextField();
+        speechInput.setPromptText("Enter your speech");
+        GridPane.setConstraints(speechInput, 1, 0);
+
+        Button addButton = new Button("Add");
+        GridPane.setConstraints(addButton, 1, 1);
+
+        grid.getChildren().addAll(label, speechInput, addButton);
+
+        Scene scene = new Scene(grid, 300, 200);*/
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
